fdb226e3f24d4d69ebe89c0b27aba397c9be2e1b
Greg Cummines <greg_cummines@starkey.com>
2015-04-15 07:18:40 -0500

    First commit

---
+++ CRC16/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CRC16
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblChecksum = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblByte1 = new System.Windows.Forms.Label();
+            this.lblByte2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(12, 36);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(546, 302);
+            this.txtData.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(38, 354);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(155, 23);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate Checksum =>";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblChecksum
+            // 
+            this.lblChecksum.AutoSize = true;
+            this.lblChecksum.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblChecksum.Location = new System.Drawing.Point(209, 359);
+            this.lblChecksum.Name = "lblChecksum";
+            this.lblChecksum.Size = new System.Drawing.Size(132, 15);
+            this.lblChecksum.TabIndex = 2;
+            this.lblChecksum.Text = "Checksum Not Calculated";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter comma delimited string of HEX values, like so: 0a,4c,3d,00,00,4d,2e,22,aa";
+            // 
+            // lblByte1
+            // 
+            this.lblByte1.AutoSize = true;
+            this.lblByte1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblByte1.Location = new System.Drawing.Point(366, 359);
+            this.lblByte1.Name = "lblByte1";
+            this.lblByte1.Size = new System.Drawing.Size(35, 15);
+            this.lblByte1.TabIndex = 2;
+            this.lblByte1.Text = "byte1";
+            // 
+            // lblByte2
+            // 
+            this.lblByte2.AutoSize = true;
+            this.lblByte2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblByte2.Location = new System.Drawing.Point(407, 359);
+            this.lblByte2.Name = "lblByte2";
+            this.lblByte2.Size = new System.Drawing.Size(35, 15);
+            this.lblByte2.TabIndex = 2;
+            this.lblByte2.Text = "byte2";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 393);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblByte2);
+            this.Controls.Add(this.lblByte1);
+            this.Controls.Add(this.lblChecksum);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtData);
+            this.Name = "MainForm";
+            this.Text = "CRC16 Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblChecksum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblByte1;
+        private System.Windows.Forms.Label lblByte2;
+    }
+}
+
---
+++ CRC16/MainForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRC16
+{
+    public partial class MainForm : Form
+    {
+        protected ushort[] CRCTbl { get; set; }
+        protected ushort CRC16 { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitCRCTable();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (txtData.Text.Length == 0)
+            {
+                MessageBox.Show("You must enter a data string!", "CRC16", MessageBoxButtons.OK);
+            }
+
+            if (!txtData.Text.Contains(","))
+            {
+                MessageBox.Show("Your data string must be comma delimited!", "CRC16", MessageBoxButtons.OK);
+            }
+
+            var data = GetData();
+            if (data.Length > 0)
+            {
+                ushort checksum = TestCRC(data);
+
+                lblChecksum.Text = checksum.ToString();
+                byte byte1 = (byte)((checksum >> 8) & 0xff);
+                byte byte2 = (byte)(checksum & 0xff);
+
+                byte[] bytes = new byte[] { byte1 };
+                lblByte1.Text = BitConverter.ToString(bytes);
+                bytes = new byte[] { byte2 };
+                lblByte2.Text = BitConverter.ToString(bytes);
+
+            }
+        }
+
+        private byte[] GetData()
+        {
+            var stringData = txtData.Text;
+            stringData = stringData.Replace(",", "");
+
+            int NumberChars = stringData.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(stringData.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        /// <summary>
+        /// Entry point for verification of the CRC-CCITT on a stream of bytes.
+        /// </summary>
+        /// <param name="stream">A byte[] of data to be calculated.</param>
+        /// <returns>The CRC for the input stream.</returns>
+        public ushort TestCRC(byte[] stream)
+        {
+            var s = new StreamWriter(@"c:\temp\crc456.txt");
+            InitCRC();
+            ushort crc = CRC16;
+            foreach (byte t in stream)
+            {
+                crc = CRC_16_CCITT(t);
+                s.WriteLine(crc);
+            }
+            s.Flush();
+            s.Close();
+            return crc;
+        }
+
+        //*********************************************************************************
+        //
+        // The following 3 functions were taken from lib_crc.c written by
+        // Lammert Bies and available from http://www.lammertbies.nl/download/lib_crc.zip
+        // as a free download with source.
+        //
+        //*********************************************************************************
+        /// <summary>
+        /// Calculate the CRC-16 value for the next byte
+        /// </summary>
+        /// <remarks>
+        /// </remarks>
+        /// <param name="next">The next byte</param>
+        /// <returns>The updated CRC value.</returns>
+        private ushort CRC_16_CCITT(byte next)
+        {
+            ushort shortC = (ushort)(0x00ff & next);
+            ushort tmp = (ushort)((CRC16 >> 8) ^ shortC);
+            CRC16 = (ushort)((CRC16 << 8) ^ CRCTbl[tmp]);
+
+            return CRC16;
+        }
+
+        /// <summary>
+        /// Initialize the CRC seed value to -1
+        /// </summary>
+        private void InitCRC()
+        {
+            CRC16 = 0xffff;
+        }
+
+        /// <summary>
+        /// Initialize the CRC table.
+        /// </summary>
+        private void InitCRCTable()
+        {
+            CRC16 = 0xffff;
+            CRCTbl = new ushort[256];
+
+            int i;
+            const ushort POLY = 0x1021;
+
+            for (i = 0; i < 256; i++)
+            {
+                ushort crc = 0;
+                ushort c = (ushort)(((ushort)i) << 8);
+                int j;
+                for (j = 0; j < 8; j++)
+                {
+                    if (((crc ^ c) & 0x8000) != 0)
+                    {
+                        crc = (ushort)((crc << 1) ^ POLY);
+                    }
+                    else
+                    {
+                        crc = (ushort)(crc << 1);
+                    }
+                    c = (ushort)(c << 1);
+                }
+
+                CRCTbl[i] = crc;
+            }
+        }
+    }
+}
